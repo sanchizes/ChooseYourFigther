b9f687032272e282a0666b6b5d5c4728f43c8667
Oleksandr Pavlyk <oleksandr.pavlyk03@gmail.com>
2024-11-21 00:04:41 +0200

    Initial commit

---
+++ KataProblems/DisemvowelTrollsTask.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChooseYourFighter.KataProblems
+{
+    public class DisemvowelTrollsTask
+    {
+        public static string DisemvowelTrolls(string str)
+        {
+            char[] vowels = {'a', 'e', 'u', 'i', 'o', 'O', 'I'};
+            char[] result = str.ToCharArray();
+            for (int i = 0; i < result.Length; i++)
+            {
+                if (vowels.Contains(result[i]))
+                {
+                    result[i] = '\0';
+                }
+            }
+            return new string(result).Replace("\0", "");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using ChooseYourFighter.KataProblems;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChooseYourFighter
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(DisemvowelTrollsTask.DisemvowelTrolls("This website is for losers LOL!"));
+            //Console.ReadKey();
+
+            //Console.WriteLine(SuperEasy("1.2"));
+
+            //Console.WriteLine(GirlsAge("8 years old"));
+
+            //Console.WriteLine(PinValidation("1234"));
+
+            Console.WriteLine(GetMiddleCharacters("james"));
+
+            Console.ReadKey();
+        }
+        //static string SuperDuperEasy(string number)
+        //{
+        //    if (double.TryParse(number, out double numValue)) return (numValue * 50 + 6).ToString();
+        //    else return "Error";
+        //}
+        //static int SuperEasy(int number)
+        //{
+        //    if (!number.ToString().Any(char.IsDigit)) return number * 2;
+        //    else return -1;
+        //}
+
+        //        static int GirlsAge(string age) => (int)Char.GetNumericValue(age[0]);
+
+        static bool PinValidation(string pin)
+        {
+            if (pin.Length == 4 || pin.Length == 6)
+            {
+                foreach (char c in pin)
+                {
+                    if (!char.IsDigit(c)) return false;
+                }
+                return true;
+            }
+            else return false;
+        }
+        static string GetMiddleCharacters(string str)
+        {
+            int len = str.Length;
+            if (!(len % 2 == 0))
+            {
+                return str[(len / 2)].ToString();
+            }
+            else
+            {
+                return str.Substring(len / 2 - 1, len / 2);
+            }
+        }
+    }
+}
